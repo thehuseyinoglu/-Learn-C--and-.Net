dbb38e5cc1bf03dbd5f047cd6e7a4ad0f6a2b861
thehuseyinoglu <thehuseyinoglu@gmail.com>
2022-12-22 00:26:03 +0300

    First Lesson

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        public void Listele(List<Musteri> musteriler)
+        {
+            foreach (var item in musteriler)
+            {
+                Console.WriteLine("Musteri Adi : " + item.Ad);
+                Console.WriteLine("Musteri Soyadi : " + item.Soyad);
+            }
+        }
+
+        public  List<Musteri> Ekle(Musteri musteri , List<Musteri> musteriler )
+        {
+            musteriler.Add(musteri);
+
+            return musteriler;
+        }
+
+        public List<Musteri> Sil(List<Musteri> musteriler , int id  )
+        {
+            foreach (var item in musteriler)
+            {
+                if ( item.Id == id) {
+                    musteriler.Remove(item);
+                    return musteriler;
+                }
+
+            }
+
+
+            return musteriler;
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri
+            {
+                Id = 1,
+                Ad = "Berkay",
+                Soyad = "Huseyinoglu"
+            };
+            Musteri musteri2 = new Musteri
+            {
+                Id = 2,
+                Ad = "onur",
+                Soyad = "Akkepenek"
+            };
+
+            List<Musteri> musteriler = new List<Musteri>();
+
+           MusteriManager result = new MusteriManager();
+
+            musteriler =  result.Ekle(musteri1, musteriler);
+            Console.WriteLine("1. musteri eklendi");
+            musteriler = result.Ekle(musteri2, musteriler);
+            Console.WriteLine("2. musteri eklendi");
+            musteriler = result.Sil(musteriler, musteri1.Id);
+            Console.WriteLine("musteri 1 sílindi");
+            result.Listele(musteriler);
+
+
+
+
+
+        }
+    }
+}
